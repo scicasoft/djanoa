d8b562615be1eb5537d9e481d33eb53dcef45379
Hassane Moustapha <hassanemoustapha@gmail.com>
2013-10-01 10:27:27 +0000

    fixed 'undefined variables in initializer' error

--- lib/generators/templates/initializer.rb
+++ lib/generators/templates/initializer.rb
@@ -1,5 +1,5 @@
 Djanoa.configure do |config|
-  config.from             = VOTRE_NUMERO_COURT
-  config.account_code     = CODE_DE_VOTRE_COMPTE
-  config.application_pass = MOT_DE_PASSE
+  config.from             = "VOTRE_NUMERO_COURT"
+  config.account_code     = "CODE_DE_VOTRE_COMPTE"
+  config.application_pass = "MOT_DE_PASSE"
 end
